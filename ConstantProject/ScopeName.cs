3ed704805f6aed184223e7bc4a3e59cfa0927315
Yehor Kadeniuk <91326774+newmasterSG@users.noreply.github.com>
2024-08-07 23:44:08 +0300

    change const value

--- ConstantProject/ScopeName.cs
+++ ConstantProject/ScopeName.cs
@@ -4,14 +4,25 @@ namespace ConstantProject
 {
     public static class ScopeName
     {
-        public const string UserManagement = "UserManagement";
+        public static class UserManagementConst
+        {
+            public const string UserManagement = "UserManagement";
 
-        public const string UserManagementName = "UserManagementService";
+            public const string UserManagementName = "UserManagementService";
 
-        public const string UserRoleDisplayName = "User Role";
+            public const string UserRoleDisplayName = "User Role";
 
-        public const string GroupName = "group";
+            public const string GroupName = "group";
 
-        public const string UserGroupDisplayName = "User group";
+            public const string UserGroupDisplayName = "User group";
+        }
+        
+        public static class GroupManagmentConst
+        {
+            public const string GroupManagment = "GroupManagment";
+
+            public const string UserManagementName = "GroupManagmentService";
+        }
+
     }
 }
